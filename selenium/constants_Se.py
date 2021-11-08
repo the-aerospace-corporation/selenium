6861c5a8bcbbb037be43fb589609360a82340631
dw28596 <don.walker@aero.org>
2021-11-08 12:08:19 -0800

    updated constants again

--- selenium/constants_Se.py
+++ selenium/constants_Se.py
@@ -2,7 +2,7 @@ import numpy as np
 import pkg_resources
 
 
-ozone_absorption_coefficients = np.loadtxt(pkg_resources.resource_filename('selenium', 'selenium/data/O3_cm2_molecule_absorption_coefficients.txt'), skiprows=1)
+ozone_absorption_coefficients = np.loadtxt(pkg_resources.resource_filename('selenium', 'data/O3_cm2_molecule_absorption_coefficients.txt'), skiprows=1)
 extended_lamda = np.arange(ozone_absorption_coefficients[-1,0]+1, 2501)
 ozone_extension = np.zeros((len(extended_lamda),2))
 ozone_extension[:,0] = extended_lamda
